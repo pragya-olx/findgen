348f6202ca5f6d19d8cfa7fdc0c92282c43bb4c0
Mayank Gupta <maygupta@C02Q1142G8WM.group.on>
2016-04-17 16:36:53 -0700

    Add employee ids

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Client.create(:name => "TCS", :location => "Noida")
-User.create(:name => "Innvoative View", :email => "admin@innov.com", :encrypted_password => "test", :role_type => "owner")
-User.create(:name => "Mark", :email => "mark@tcs.com", :encrypted_password => "test", :role_type => "admin", :client_id => 1)
+c = Client.create(:name => "TCS", :location => "Noida")
+User.create(:name => "Innvoative View", :email => "admin@innov.com", :encrypted_password => "test", :role_type => "owner", :employee_id => "1")
+User.create(:name => "Mark", :email => "mark@tcs.com", :encrypted_password => "test", :role_type => "admin", :client_id => c.id, :employee_id => "2")
 
-User.create(:name => "Jasmine", :email => "jasmine@tcs.com", :encrypted_password => "test", :role_type => "approver", :client_id => 1)
+User.create(:name => "Jasmine", :email => "jasmine@tcs.com", :encrypted_password => "test", :role_type => "approver", :client_id => c.id, :employee_id => "3")
 
-User.create(:name => "Carl", :email => "carl@tcs.com", :encrypted_password => "test", :role_type => "spoc", :client_id => 1)
+User.create(:name => "Carl", :email => "carl@tcs.com", :encrypted_password => "test", :role_type => "spoc", :client_id => c.id, :employee_id => "4")
 
 User.create(:name => "Luminous", :email => "lum@vendor.com", :role_type => "vendor")
 User.create(:name => "Bajaj", :email => "bajaj@vendor.com", :role_type => "vendor")
