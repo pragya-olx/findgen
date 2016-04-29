8a8ee3fc98414ef249da4ad855b1f15f01691989
Mayank Gupta <mayank@Mayanks-MacBook-Air.local>
2016-04-28 22:36:40 -0700

    Updated aws keys

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 Aws.config.update({
   region: 'us-east-1',
-  credentials: Aws::Credentials.new("AKIAJK5EH56GDJHQUI4A", "yBFYohL4CQsVbnNO+vREc9hj0UM9xiO01Zq5amnr"),
+  credentials: Aws::Credentials.new("AKIAJK4ULXARMVZ4A6SA", "n1g0C0ev8ANpWH2yOJ3oj6ZLrp9doiwLABVtPyqv"),
 })
 
 ENV['S3_BUCKET_NAME'] = 'findgen-dev' unless Rails.env.production?
