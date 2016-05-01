2e99956cbb9bcb4edac0416f519da818a7d453e4
Mayank Gupta <mayank@Mayanks-MacBook-Air.local>
2016-04-30 23:04:27 -0700

    login_error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
-      redirect_to '/'
+      redirect_to '/', :flash => { :notice  => "Welcome #{user.name}" }
     else
       # Create an error message.
-      flash[:danger] = 'Invalid email/password combination' 
-      render 'new'
+      redirect_to '/', :flash => { :error  => "'Invalid email/password combination' " }
     end
   end
 
