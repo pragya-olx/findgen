eb31003393cfe9bc73e64e15082809791a0a329d
Mayank Gupta <mayank@Mayanks-MacBook-Air.local>
2016-05-02 23:34:06 -0700

    redirect to login if user doesn't exist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  before_action :require_login
+
+  def require_login
+    if current_user.nil?
+      redirect_to '/login'
+    end
+  end
 
   def states
     CS.states(:IN)
