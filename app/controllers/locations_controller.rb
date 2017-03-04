458581d8d3b40e94ef5fbe26d541cff518eb08ac
Pragya Gupta <pragya.gupta@olx.com>
2017-03-04 16:56:14 +0530

    fixes

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -15,19 +15,13 @@ class LocationsController < ApplicationController
 
   def create
     @location = Location.where(:state => params[:state]).first
-    if @location.nil?
+   
         @location = Location.new(params.permit(:state, :kva_30_day, :kva_70_day, :kva_130_day, :kva_250_day, :kva_30_hour, :kva_70_hour, :kva_130_hour, :kva_250_hour))
-      end
+    
       @location.save
       render json: {}, status: 201 
     end
 
-    def update
-      location = Location.find(params[:id])
-      location.update!(location_params)
-
-      redirect_to '/locations'
-    end
 
     def location_params
       params.require(:location).permit(:kva_30_day, :kva_70_day, :kva_130_day, :kva_250_day, :kva_30_hour, :kva_70_hour, :kva_130_hour, :kva_250_hour)
