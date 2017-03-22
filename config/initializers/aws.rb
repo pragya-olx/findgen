2a9b5fa686156c06d7238e0ad3cba8833135f7b7
Pragya Gupta <pragya.gupta@olx.com>
2017-03-22 21:07:44 +0530

    removed unless

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,4 +3,4 @@ Aws.config.update({
   credentials: Aws::Credentials.new("AKIAJK4ULXARMVZ4A6SA", "n1g0C0ev8ANpWH2yOJ3oj6ZLrp9doiwLABVtPyqv"),
 })
 
-ENV['S3_BUCKET_NAME'] = 'findgen-dev' unless Rails.env.production?
+ENV['S3_BUCKET_NAME'] = 'findgen-dev'
